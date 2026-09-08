215da11432f77a0626bd156ba8d018c1c85caf5d
User <user@example.com>
2026-09-08 16:09:12 -0300

    se termino la tarea

--- ejercicios/04-isp.ts
+++ ejercicios/04-isp.ts
@@ -1,10 +1,19 @@
 interface MultifunctionPrinter {
+}
+
+interface Printer {
   print(document: string): void;
+};
+
+interface Scanner {
   scan(document: string): void;
+}
+
+interface Fax {
   fax(document: string): void;
 }
 
-class SimplePrinter implements MultifunctionPrinter {
+class SimplePrinter implements Printer {
   print(document: string): void {
     console.log(`Imprimiendo: ${document}`);
   }
--- ejercicios/05-dip.ts
+++ ejercicios/05-dip.ts
@@ -1,16 +1,26 @@
-class EmailSender {
+interface Notifier {
+  send(to: string, message: string): void;
+}
+
+class EmailSender implements Notifier {
   send(to: string, message: string): void {
     console.log(`Correo para ${to}: ${message}`);
   }
 }
 
 class OrderService {
+  private notifier: Notifier;
+
+  constructor(notifier: Notifier) {
+    this.notifier = notifier;
+  }
+
   createOrder(customerEmail: string): void {
     console.log("Pedido creado");
-
-    const emailSender = new EmailSender();
-    emailSender.send(customerEmail, "Tu pedido fue creado");
+    this.notifier.send(customerEmail, "Tu pedido fue creado");
   }
 }
 
-new OrderService().createOrder("ana@example.com");
+const emailNotifier = new EmailSender();
+const service = new OrderService(emailNotifier);
+service.createOrder("ana@example.com");
